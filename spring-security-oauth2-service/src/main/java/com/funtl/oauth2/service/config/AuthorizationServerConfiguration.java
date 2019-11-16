d33dbcff069f695b3ce3ea3716eb68b65a56dec3
YuweiZhan <winzhanyuwei@163.com>
2019-11-16 22:59:11 +0800

    基于内存存储令牌

--- spring-security-oauth2-service/src/main/java/com/funtl/oauth2/service/config/AuthorizationServerConfiguration.java
+++ spring-security-oauth2-service/src/main/java/com/funtl/oauth2/service/config/AuthorizationServerConfiguration.java
@@ -1,6 +1,8 @@
 package com.funtl.oauth2.service.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -18,12 +20,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
                 .withClient("client")
-                .secret("secret")
+                .secret(passwordEncoder.encode("secret"))
                 .authorizedGrantTypes("authorization_code")
                 .scopes("app")
                 .redirectUris("http://www.funtl.com");
---
+++ spring-security-oauth2-service/src/main/java/com/funtl/oauth2/service/config/WebSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.funtl.oauth2.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+import java.security.PublicKey;
+
+/**
+ * Desription:
+ *
+ * @ClassName WebSecurityConfiguration
+ * @Author Zhanyuwei
+ * @Date 2019/11/16 22:28
+ * @Version 1.0
+ **/
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("123456")).roles("ADMIN")
+                .and()
+                .withUser("user").password(passwordEncoder().encode("123456")).roles("USER");
+    }
+}
